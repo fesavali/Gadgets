68102354fec6020a624d693302e06918a0cfe521
fesavali <felixnzioki99@gmail.com>
2022-04-07 13:21:14 +0300

    check reviews

---
+++ c_reviews.php
@@ -0,0 +1,63 @@
+<?php
+include('header.html'); 
+include('conf.php');
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM posts WHERE id = '$id'";
+$sql1 = "SELECT * FROM reviews WHERE post_id = '$id' ORDER BY id DESC LIMIT 5";
+$result = mysqli_query($conf,$sql);
+$result1 = mysqli_query($conf,$sql1);
+?>
+  <div class="row" style="padding-bottom:10px;padding-top: 10px;">
+  <div class="col-md-3"></div>
+  <div class="col-md-6" style="color:#000;">
+  	<?php
+  	if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+	?>
+  	<div class="card">
+  <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light" style="padding-left: 120px;">
+    <img src="<?php echo "images/".$row["image"];?>" class="img-fluid" style="height:230px;"/>
+    <a href="#!">
+      <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+    </a>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Gadget Brand: &nbsp;&nbsp;<b><?php echo $row["brand"]; ?></b></h5>
+    <h5 class="card-title">Gadget Type: &nbsp;&nbsp;<b><?php echo ucwords($row["type"]); ?></b></h5>
+    <h5 class="card-title">Gadget Price: &nbsp;&nbsp;<b><span>&#36;</span>&nbsp;&nbsp;<?php echo number_format($row["price"], 2); ?></b></h5>
+    <h5 class="card-title">Gadget RAM: &nbsp;&nbsp;<b><?php echo $row["ram"]; ?>&nbsp;&nbsp;GB</b></h5>
+    <h5 class="card-title">Gadget Storage: &nbsp;&nbsp;<b><?php echo $row["storage"]; ?>&nbsp;&nbsp;GB</b></h5>
+    <h5 class="card-title">Reviews:</h5>
+    <?php
+  	if (mysqli_num_rows($result1) > 0) {
+    // output data of each row
+    while($row1 = mysqli_fetch_assoc($result1)) {
+	?>
+    <p class="card-text"><?php echo "<b>".$row1["name"]."</b>"." :".$row1["cemment"]; ?></p>
+    <?php
+  }
+} else {
+    echo "No Reviews posted yet"; 
+}
+mysqli_close($conf);
+  ?>
+  </div>
+  <div class="card-footer text-muted">Gadget Added On:<b><?php echo $row["added_on"]; ?></b></div>
+</div>
+  <br/>
+  </div>
+  <?php
+   }
+} else {
+    echo "No Item posted yet"; 
+}
+
+  ?>
+
+  <div class="col-md-3"></div>
+</div>
+<?php
+include('footer.html'); 
+?>
