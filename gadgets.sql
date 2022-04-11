0e91a81c705d5c7641ec7ad70b8149817dec7dd6
fesavali <felixnzioki99@gmail.com>
2022-04-11 09:53:01 +0300

    add orders table

--- gadgets.sql
+++ gadgets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2022 at 12:07 PM
+-- Generation Time: Apr 10, 2022 at 05:16 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.4.21
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `gadgets`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(155) NOT NULL,
+  `post_id` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `order_time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `post_id`, `quantity`, `name`, `location`, `email`, `number`, `order_time`) VALUES
+(1, '1', '3', 'saval', 'Las Vegas', 'saval@gmail.com', '37764849575', '2022-04-10 14:48:39.747084'),
+(2, '1', '2', 'saval', 'NewYork', 'sasa@buyer.com', '67488565', '2022-04-10 14:52:11.053237');
 
 -- --------------------------------------------------------
 
@@ -74,6 +99,12 @@ INSERT INTO `reviews` (`id`, `post_id`, `cemment`, `name`) VALUES
 --
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
@@ -88,6 +119,12 @@ ALTER TABLE `reviews`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(155) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `posts`
